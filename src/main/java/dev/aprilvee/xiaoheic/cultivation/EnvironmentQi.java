62790d72a28adb61aa6479b4399b6378421472dc
vvyzen <seekodestroyer@gmail.com>
2023-08-05 00:22:21 -0500

    so many tag woa

--- src/main/java/dev/aprilvee/xiaoheic/cultivation/EnvironmentQi.java
+++ src/main/java/dev/aprilvee/xiaoheic/cultivation/EnvironmentQi.java
@@ -2,10 +2,11 @@ package dev.aprilvee.xiaoheic.cultivation;
 
 import dev.aprilvee.xiaoheic.registry.tags;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.tags.ITagManager;
 
 public class EnvironmentQi {
 
@@ -16,7 +17,7 @@ public class EnvironmentQi {
         if(diameter % 2 != 0){
             radius = (diameter - 1)/2;
         }else{radius = diameter/2;}
-        BlockPos starterPos = pos.offset(radius,radius,radius);;
+        BlockPos starterPos = pos.offset(radius,radius,radius);
         BlockPos currentPos = starterPos;
 
         //level.getBlockState(currentPos).getBlock();
@@ -24,7 +25,7 @@ public class EnvironmentQi {
         for(int x = 0; x < diameter; x++){ //forward to back
             for(int y = 0; y < diameter; y++){ //top to bottom
                 for(int z = 0; z < diameter; z++) { //left to right
-                    result = getSpirit(level.getBlockState(currentPos).getBlock());
+                    result = getSpirit(currentPos, level);
                     qi[0] += result[0];
                     qi[1] += result[1];
                     qi[2] += result[2];
@@ -32,8 +33,6 @@ public class EnvironmentQi {
                     qi[4] += result[4];
                     qi[5] += result[5];
                     qi[6] += result[6];
-                    level.getServer().getPlayerList().getPlayerByName("Dev").sendSystemMessage(Component.literal(
-                            currentPos.toShortString()));
                     currentPos = currentPos.offset(0, 0 , -1);
                     //shift block z
                 }
@@ -46,40 +45,50 @@ public class EnvironmentQi {
         return qi;
     }
 
-    public static float[] getSpirit(Block block){
+    public static float[] getSpirit(BlockPos pos, Level level){
         //i think that i am commiting crimes
-        float[] qi = new float[7];
-        if(ForgeRegistries.BLOCKS.tags().getTag(tags.spiritstrong).contains(block)){ qi[0] = 5;
-        } else if(ForgeRegistries.BLOCKS.tags().getTag(tags.spiritweak).contains(block)){ qi[0] = 0.5f;
-        }
+        Block block = level.getBlockState(pos).getBlock();
+        Fluid fluid = level.getFluidState(pos).getType();
+        float[] qi = {0, 0, 0, 0, 0, 0, 0};
+        ITagManager<Block> tag = ForgeRegistries.BLOCKS.tags();
+        ITagManager<Fluid> fluidtag = ForgeRegistries.FLUIDS.tags();
 
-        if(ForgeRegistries.BLOCKS.tags().getTag(tags.metalstrong).contains(block)){ qi[1] = 5;
-        } else if(ForgeRegistries.BLOCKS.tags().getTag(tags.metal).contains(block)){ qi[1] = 2;
-        } else if(ForgeRegistries.BLOCKS.tags().getTag(tags.metalweak).contains(block)){ qi[1] = 0.5f;
-        }
+            if(tag.getTag(tags.spiritvstrong).contains(block)){ qi[0] = 15;
+            } else if(tag.getTag(tags.spiritstrong).contains(block)){ qi[0] = 5;
+            } else if(tag.getTag(tags.spirit).contains(block)){ qi[0] = 2;
+            } else if(tag.getTag(tags.spiritweak).contains(block)){ qi[0] = 0.25f;
+            }
 
-        if(ForgeRegistries.BLOCKS.tags().getTag(tags.waterstrong).contains(block)){qi[2] = 5;
-        } else if(ForgeRegistries.BLOCKS.tags().getTag(tags.water).contains(block)){qi[2] = 2;
-        } else if(ForgeRegistries.BLOCKS.tags().getTag(tags.waterweak).contains(block)){qi[2] = 0.5f;
-        }
+            if(tag.getTag(tags.metalstrong).contains(block)){ qi[1] = 5;
+            } else if(tag.getTag(tags.metal).contains(block)){ qi[1] = 2;
+            } else if(tag.getTag(tags.metalweak).contains(block)){ qi[1] = 0.25f;
+            }
 
-        if(ForgeRegistries.BLOCKS.tags().getTag(tags.woodstrong).contains(block)){qi[3] = 5;
-        } else if(ForgeRegistries.BLOCKS.tags().getTag(tags.wood).contains(block)){qi[3] = 2f;
-        } else if(ForgeRegistries.BLOCKS.tags().getTag(tags.woodweak).contains(block)){qi[3] = 0.5f;
-        }
+            if(tag.getTag(tags.waterstrong).contains(block)){qi[2] = 5;
+            } else if(tag.getTag(tags.water).contains(block)){qi[2] = 2;
+            } else if(tag.getTag(tags.waterweak).contains(block)){qi[2] = 0.25f;
+            }
 
-        if(ForgeRegistries.BLOCKS.tags().getTag(tags.firestrong).contains(block)){qi[4] = 5;
-        } else if(ForgeRegistries.BLOCKS.tags().getTag(tags.fire).contains(block)){qi[4] = 2;
-        } else if(ForgeRegistries.BLOCKS.tags().getTag(tags.fireweak).contains(block)){qi[4] = 0.5f;
-        }
+            if(tag.getTag(tags.woodstrong).contains(block)){qi[3] = 5;
+            } else if(tag.getTag(tags.wood).contains(block)){qi[3] = 2f;
+            } else if(tag.getTag(tags.woodweak).contains(block)){qi[3] = 0.25f;
+            }
 
-        if(ForgeRegistries.BLOCKS.tags().getTag(tags.earthstrong).contains(block)){qi[5] = 5;
-        } else if(ForgeRegistries.BLOCKS.tags().getTag(tags.earth).contains(block)){qi[5] = 2;
-        } else if(ForgeRegistries.BLOCKS.tags().getTag(tags.earthweak).contains(block)){qi[5] = 0.5f;
-        }
+            if(tag.getTag(tags.firestrong).contains(block)){qi[4] = 5;
+            } else if(tag.getTag(tags.fire).contains(block)){qi[4] = 2;
+            } else if(tag.getTag(tags.fireweak).contains(block)){qi[4] = 0.25f;
+            }
 
+            if(tag.getTag(tags.earthstrong).contains(block)){qi[5] = 5;
+            } else if(tag.getTag(tags.earth).contains(block)){qi[5] = 2;
+            } else if(tag.getTag(tags.earthweak).contains(block)){qi[5] = 0.25f;
+            }
 
+            if(fluidtag.getTag(tags.waterfluid).contains(fluid)){qi[2] += 0.25f;
+            }else if(fluidtag.getTag(tags.lava).contains(fluid)){qi[4] += 2;
+            }
 
+        if(tag.getTag(tags.solid).contains(block)){qi[6] = 1;} //crampedness
         return qi;
     }
 }
--- src/main/java/dev/aprilvee/xiaoheic/registry/tags.java
+++ src/main/java/dev/aprilvee/xiaoheic/registry/tags.java
@@ -3,12 +3,19 @@ package dev.aprilvee.xiaoheic.registry;
 import dev.aprilvee.xiaoheic.main;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.Fluid;
 
 public class tags {
+
+    public static final TagKey<Block> solid = BlockTags.create(new ResourceLocation(main.MODID,"solid"));
+
     public static final TagKey<Block> spiritweak = BlockTags.create(new ResourceLocation(main.MODID,"spiritweak"));
+    public static final TagKey<Block> spirit = BlockTags.create(new ResourceLocation(main.MODID,"spirit"));
     public static final TagKey<Block> spiritstrong = BlockTags.create(new ResourceLocation(main.MODID,"spiritstrong"));
+    public static final TagKey<Block> spiritvstrong = BlockTags.create(new ResourceLocation(main.MODID,"spiritvstrong"));
 
     public static final TagKey<Block> metalweak = BlockTags.create(new ResourceLocation(main.MODID,"metalweak"));
     public static final TagKey<Block> metal = BlockTags.create(new ResourceLocation(main.MODID,"metal"));
@@ -29,4 +36,8 @@ public class tags {
     public static final TagKey<Block> earthweak = BlockTags.create(new ResourceLocation(main.MODID,"earthweak"));
     public static final TagKey<Block> earth = BlockTags.create(new ResourceLocation(main.MODID,"earth"));
     public static final TagKey<Block> earthstrong = BlockTags.create(new ResourceLocation(main.MODID,"earthstrong"));
+
+    public static final TagKey<Fluid> waterfluid = FluidTags.create(new ResourceLocation("minecraft","water"));
+    public static final TagKey<Fluid> lava = FluidTags.create(new ResourceLocation("minecraft","water"));
+
 }
